d532beecf80adb686f6be90bb274ebad6de1ce2f
Amit Skarbin <amitskarbin1@gmail.com>
2023-12-13 19:57:28 +0200

    stone3_prefinal

--- GUI/teacher_gui.py
+++ GUI/teacher_gui.py
@@ -13,12 +13,14 @@ class ServerGUI:
         self.window = tk.Tk()
         self.window.title("Teacher's Dashboard")
         self.resolution = resolution
+        self.aspect_ratio = resolution[1] / resolution[0]
         self.student_frames = {}
         self.setup_gui()
 
+
     def setup_gui(self):
         self.streams_frame = Frame(self.window)
-        self.streams_frame.pack(fill=tk.BOTH, expand=True)
+        self.streams_frame.pack(fill=tk.BOTH, expand=True)  # Use pack here for the main frame only
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.window.bind('<Configure>', self.on_window_resize)  # Bind the resize event
         self.start_server()
@@ -39,72 +41,77 @@ class ServerGUI:
 
     def add_student_stream(self, student_id):
         frame_label = Label(self.streams_frame)
-        frame_label.pack(fill=tk.BOTH, expand=True)
+        # Use grid to manage the layout of student frames within self.streams_frame
+        # Initially place the frame out of view; it will be positioned by update_layout
+        frame_label.grid(row=999, column=999)
         self.student_frames[student_id] = frame_label
         self.update_layout()
 
     def new_frame_received(self, client_address, frame):
         student_id = f"{client_address[0]}:{client_address[1]}"
-        if frame is None:
-            # If frame is None, remove the student's frame because they have disconnected
-            self.remove_student_stream(student_id)
-        else:
-            if student_id not in self.student_frames:
-                self.add_student_stream(student_id)
-            self.update_video_display(student_id, frame)
+        if student_id not in self.student_frames:
+            self.add_student_stream(student_id)
+        self.update_video_display(student_id, frame)
+        self.update_layout()
 
     def remove_student_stream(self, student_id):
         if student_id in self.student_frames:
             frame_label = self.student_frames[student_id]
             frame_label.grid_forget()  # Remove the frame from the grid
             frame_label.destroy()  # Destroy the frame widget
-            del self.student_frames[student_id]  # Remove the reference from the dictionary
-            self.update_layout()  # Update the layout since the number of frames has changed
+            del self.student_frames[student_id]  # Remove the reference
+            self.update_layout()  # Update the layout since a stream was removed
 
     def update_video_display(self, student_id, frame):
-        # Convert the frame from BGR to RGB
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Convert the frame to a format that Tkinter can use
-        frame_image = Image.fromarray(frame_rgb)
-        frame_photo = ImageTk.PhotoImage(image=frame_image)
-        # Update the Label widget with the new image
-        if student_id in self.student_frames:
+        if frame is not None:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_image = Image.fromarray(frame)
+            frame_photo = ImageTk.PhotoImage(image=frame_image)
             self.student_frames[student_id].configure(image=frame_photo)
-            self.student_frames[student_id].image = frame_photo  # Keep a reference to prevent garbage-collection
+            self.student_frames[student_id].image = frame_photo  # Keep a reference
+        else:
+            # Handle the case where the client has disconnected
+            self.remove_student_stream(student_id)
 
     def update_layout(self):
         num_students = len(self.student_frames)
         if num_students == 0:
-            return  # Avoid division by zero when there are no students
+            return  # Exit if there are no student streams
 
-        # Get the new window size
+        # Get the window's current width and height
         window_width = self.streams_frame.winfo_width()
         window_height = self.streams_frame.winfo_height()
 
-        # Determine the number of rows and columns, with a maximum of 5 columns
-        cols = min(5, num_students)
+        # Calculate the appropriate number of columns and rows
+        cols = min(5, num_students)  # Set the maximum number of columns
         rows = math.ceil(num_students / cols)
 
-        # Calculate the size of each frame, maintaining the aspect ratio
-        aspect_ratio = self.resolution[1] / self.resolution[0]  # height / width
+        # Calculate the size of each frame
         frame_width = window_width // cols
-        frame_height = int(frame_width * aspect_ratio)
+        frame_height = int(frame_width * self.aspect_ratio)  # Maintain the aspect ratio
+
+        # Check if the total height exceeds the window height and adjust if necessary
+        total_height = frame_height * rows
+        if total_height > window_height:
+            frame_height = window_height // rows
+            frame_width = int(frame_height / self.aspect_ratio)
 
         # Update the size and position of each frame
         for i, (student_id, frame_label) in enumerate(self.student_frames.items()):
             row = i // cols
             col = i % cols
             frame_label.grid(row=row, column=col, sticky='nsew')
-            frame_label.config(width=frame_width, height=frame_height)  # This sets the size of the frame
 
-        # Configure weight for each row and column to make them expandable
+            # Remove any previous photo images to avoid holding onto deleted references
+            frame_label.image = None
+
+        # Adjust the configuration of each grid cell
         for r in range(rows):
             self.streams_frame.grid_rowconfigure(r, weight=1)
         for c in range(cols):
             self.streams_frame.grid_columnconfigure(c, weight=1)
-
     def calculate_grid_dimensions(self, num_students):
-        cols = min(5, math.ceil(math.sqrt(num_students)))
+        cols = min(5, math.ceil(math.sqrt(num_students)))  # Limit the number of columns to a maximum of 5
         rows = math.ceil(num_students / cols)
         return rows, cols
 
